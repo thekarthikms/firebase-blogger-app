634d19a14bab32e2a3223828ec153410b649a302
=karthikms-msil <=karthikms-msil@github.com>
2021-02-08 17:48:51 +0530

    1. Created UI

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry:"./src/index.js",
+    plugins:[
+        new CleanWebpackPlugin()
+    ]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const { merge }= require('webpack-merge')
+const config = require('./webpack.config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(config,{
+    mode:"development",
+    output:{
+        filename:"main.js",
+        path:path.resolve(__dirname,'dev')
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        })
+    ],
+    module:{
+        rules:[{
+            test:/\.scss$/,
+            use:['style-loader','css-loader','sass-loader']
+        }]
+    },
+})
