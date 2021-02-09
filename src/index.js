d128e35589865a5fb87f8682110a81c503de4635
=karthikms-msil <=karthikms-msil@github.com>
2021-02-09 11:52:56 +0530

    1.Functions to Upload,Clear and GetData 2.Seperated Files to utils.js 3.Optimized

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import "./css/main.scss";
 import firebase from 'firebase/app';
 import 'firebase/firestore'
-
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAt9rnsVs8frEjKh2FZTKlIWBh_vt_mbu0",
-    authDomain: "fir-blog-app-85c1f.firebaseapp.com",
-    projectId: "fir-blog-app-85c1f",
-    storageBucket: "fir-blog-app-85c1f.appspot.com",
-    messagingSenderId: "445622570987",
-    appId: "1:445622570987:web:319f3de458edd8cb7393ae"
-};
+import {
+    removeAllChildNodes,
+    showPrevBlogs,
+    showError,
+    showLoader
+} from './js/utils'
+import {
+    firebaseConfig
+} from './firebase.config'
 
 firebase.initializeApp(firebaseConfig);
 
 
 const form = document.querySelector('#add-blog')
-const uploadF = document.querySelector('#uploadfield')
+const uploadForm = document.querySelector('#uploadfield')
 const prevBlogs = document.querySelector('#previous-blogs')
-let prevContent = document.querySelector('#previous-content')
-let clearPrev = document.querySelector('#clear-prev')
-let loader = document.querySelector('.loader-container')
+let prevContent = document.querySelector('#previous-content'),
+    clearPrev = document.querySelector('#clear-prev'),
+    loader = document.querySelector('.loader-container'),
+    loaderCircle = document.querySelector('.loader'),
+    clearField = document.querySelector('#clearfield'),
+    username = document.querySelector('#username'),
+    blog = document.querySelector('#blog-data'),
+    blogcontainer = document.querySelector('#blog-container'),
+    oldUser = ''
+
 const db = firebase.firestore();
 
 db.collection('blogs').get().then((snapshot) => {
@@ -31,64 +35,57 @@ db.collection('blogs').get().then((snapshot) => {
     })
 })
 
-
-uploadF.addEventListener('click', () => {
-    let username = form.username.value;
-    let blog = form.blog.value;
-    if (username && blog) {
-        if (username.length > 3 && blog.length > 6) {
-            console.log(username)
-            console.log(blog)
+//form upload function
+uploadForm.addEventListener('click', () => {
+    let nameData = form.username.value;
+    let blogData = form.blog.value;
+    let timeData = new Date();
+    if (nameData && blogData) {
+        if (nameData.length > 3 && blogData.length > 6) {
+            oldUser = nameData
             db.collection('blog').add({
-                name: username,
-                blog: blog
+                name: nameData,
+                blog: blogData,
+                time: timeData.toLocaleString()
             })
-        }else{
-            
+            showLoader(loader)
+        } else {
+            showError('Enter Minimum required values', loaderCircle, loader)
         }
+    } else {
+        showError('Enter Data to upload blog', loaderCircle, loader)
     }
-    clearData()
+    form.blog.value = ''
 })
 
-prevBlogs.addEventListener('click',()=>{
+//show previous blog function
+prevBlogs.addEventListener('click', () => {
     removeAllChildNodes(prevContent)
-    let blogcontainer = document.querySelector('#blog-container')
-    
-    blogcontainer.style.display = 'flex'
-    db.collection('blog').get().then((snapshot) => {
-        snapshot.docs.forEach(doc =>{
-            showPrevBlogs(doc)
+    l
+    if (oldUser) {
+        blogcontainer.style.display = 'flex'
+        db.collection('blog').where('name', '==', oldUser).get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                showPrevBlogs(doc, prevContent)
+            })
+
         })
-        
-    })
- 
-})
-
-const showPrevBlogs = (data)=>{
-    let post = document.createElement('div')
-        post.setAttribute('class','post')
-        let h2 = document.createElement('h2')
-        let p = document.createElement('p')
-        h2.innerHTML = `${data.data().blog}`
-        p.innerHTML = `Blog by : ${data.data().name}`
-        console.log(data.name)
-        post.append(h2)
-        post.append(p)
-        prevContent.append(post)
-}
-
-clearPrev.addEventListener('click',()=>{
-    let bc = document.querySelector('#blog-container')
-    bc.style.display = 'none'
-})
-
-let removeAllChildNodes = (parent) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+    } else {
+        showError("No data found...Enter your blog", loaderCircle, loader)
     }
-}
 
-let clearData = ()=>{
-    document.querySelector('#username').value = ''
-    document.querySelector('#blog-data').value = ''
-}
+})
+
+
+//clear previous data function
+clearPrev.addEventListener('click', () => {
+    blogcontainer.style.display = 'none',
+        username.value = ''
+    blog.value = ''
+})
+
+//clear field button function
+clearField.addEventListener('click', () => {
+    username.value = ''
+    blog.value = ''
+})
---
+++ src/js/utils.js
@@ -0,0 +1,50 @@
+let removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}//remove child nodes function
+
+const showPrevBlogs = (data,prevContent ) => {
+    let post = document.createElement('div')
+    post.setAttribute('class', 'post')
+    let h2 = document.createElement('h2')
+    let p = document.createElement('p')
+    let p2 = document.createElement('p')
+    h2.innerHTML = `${data.data().blog}`
+    p.innerHTML = `Blog by : ${data.data().name}`
+    p2.innerHTML = `Time : ${data.data().time}`
+    console.log(data.name)
+    post.append(h2)
+    post.append(p)
+    post.append(p2)
+    prevContent.append(post)
+}//show previous blogs function
+
+let showError = (err,loaderCircle,loader) => {
+    loaderCircle.style.display = 'none'
+    loader.style.display = 'flex'
+    let h1 = document.createElement('h1')
+    h1.setAttribute('style', 'font-size:3rem;color:black;')
+    h1.innerHTML = err
+    loader.append(h1)
+    setTimeout(() => {
+        loader.style.display = 'none'
+        loader.removeChild(h1)
+        loaderCircle.style.display = 'block'
+        console.log('after2s')
+    }, 2000);
+}//show error function
+
+let showLoader = (loader) => {
+    loader.style.display = 'flex'
+    setTimeout(() => {
+        loader.style.display = 'none'
+    }, 2000);
+}//show loader function
+
+export {
+    removeAllChildNodes,
+    showPrevBlogs,
+    showError,
+    showLoader
+}
